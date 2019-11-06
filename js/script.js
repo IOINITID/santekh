59dfe128aba1c870c5e1974587c6e9fa1e084eeb
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-11-06 15:26:29 +0300

    Update project files

--- js/script.js
+++ js/script.js
@@ -5,8 +5,46 @@ $('.menu-button').on('click', () => {
 
 $('.navigation__item').mouseover(() => {
   // $('.navigation--hover').addClass('show');
+  $('.main__shadow').removeClass('hide');
 });
 
 $('.navigation__item').mouseout(() => {
   // $('.navigation--hover').removeClass('show');
+  $('.main__shadow').addClass('hide');
+});
+
+$('.slider-one').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: true,
+  // responsive: [{
+  //   breakpoint: 768,
+  //   settings: {
+  //     arrows: true,
+  //     slidesToShow: 1,
+  //     dots: false
+  //   }
+  // }]
+});
+
+$('.slider-two').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 5000,
+  arrows: true,
+  dots: false,
+  prevArrow: '.discounts__controls-prev',
+  nextArrow: '.discounts__controls-next',
+  // responsive: [{
+  //   breakpoint: 768,
+  //   settings: {
+  //     arrows: true,
+  //     slidesToShow: 1,
+  //     dots: false
+  //   }
+  // }]
 });
