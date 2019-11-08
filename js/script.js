1a8e482de60c9d76fee0a42ea88ca8e9d233f35c
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-11-08 16:04:20 +0300

    Added list page

--- js/script.js
+++ js/script.js
@@ -103,3 +103,15 @@ $('.slider-five').slick({
   //   }
   // }]
 });
+
+$("#slider").slider({
+  animate: "slow",
+  min: 0,
+  max: 50000,
+  range: true,
+  values: [10000, 30000],
+  slide: function (event, ui) {
+    $('#slider__value-from').val(ui.values[0]);
+    $('#slider__value-to').val(ui.values[1]);
+  }
+});
